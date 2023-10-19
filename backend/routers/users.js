879272a68d4694ebb4900dde3368c090eb1866ca
MUNIF <munif@shellbeehaken.com>
2023-10-19 17:03:26 +0600

    47. Get User and List of Users Excluding Password

--- backend/routers/users.js
+++ backend/routers/users.js
@@ -9,6 +9,26 @@ router.get(`/`, async (req, res) => {
   res.status(200).send({ success: true, usersList });
 });
 
+// Get a user by ID
+router.get("/:id", async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    // Find the category by ID
+    const user = await User.findById(userId).select("-password");
+
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ message: "Category not found" });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "An error occurred while retrieving the user" });
+  }
+});
+
 // create a User
 router.post(`/`, async (req, res) => {
   let user = new User({
