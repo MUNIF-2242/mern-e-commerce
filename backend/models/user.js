e8fc65b55cd3a74cd67aab52ab65e7d7100bd3ff
MUNIF <munif@shellbeehaken.com>
2023-10-19 15:40:04 +0600

    44. User Models and Schemas

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,19 +2,70 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  createdDate: {
-    type: Date,
-    default: Date.now(),
-  },
-  createdBy: {
+  name: {
     type: String,
+    required: true,
   },
-  modifiedDate: {
-    type: Date,
-  },
-  modifiedBy: {
+  email: {
     type: String,
+    required: true,
   },
+  password: {
+    type: String,
+    required: true,
+  },
+  street: {
+    type: String,
+    default: "",
+  },
+  apartment: {
+    type: String,
+    default: "",
+  },
+  city: {
+    type: String,
+    default: "",
+  },
+  zip: {
+    type: String,
+    default: "",
+  },
+
+  country: {
+    type: String,
+    default: "",
+  },
+  phone: {
+    type: Number,
+    default: "",
+  },
+  isAdmin: {
+    type: Boolean,
+    required: true,
+  },
+
+  // createdDate: {
+  //   type: Date,
+  //   default: Date.now(),
+  // },
+  // createdBy: {
+  //   type: String,
+  // },
+  // modifiedDate: {
+  //   type: Date,
+  // },
+  // modifiedBy: {
+  //   type: String,
+  // },
+});
+
+// Create a virtual 'id' field
+userSchema.virtual("id").get(function () {
+  return this._id.toHexString();
+});
+
+userSchema.set("toJSON", {
+  virtuals: true,
 });
 
 // Model
